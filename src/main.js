c69e1e7f791019bec33de62b7456c844e45856ca
Daniel Berrones <Daniel.A.Berrones@gmail.com>
2020-10-23 23:45:47 -0500

    Refactored code

--- src/main.js
+++ src/main.js
@@ -1,24 +1,26 @@
 function getString() {
-    const userInput = prompt("Type a string: ");
-    return userInput;
+    var userInput = document.getElementById("userText").value;
+    
+    document.getElementById("outputText").innerHTML = userInput;
+    // alert(userInput);
 }
 
-const userInput = getString();
+// const userInput = getString();
 
-function countVowels(str) {
-    const re = /[aeiou]/ig;
-    const totalVowels = str.match(re);
+// function countVowels(str) {
+//     const re = /[aeiou]/ig;
+//     const totalVowels = str.match(re);
 
-    if (totalVowels === null) {
-        return `Please enter a string.`
+//     if (totalVowels === null) {
+//         return `Please enter a string.`
 
-    } else if (totalVowels.length === 1) {
-        return `There is 1 vowel in "${str}"`;
+//     } else if (totalVowels.length === 1) {
+//         return `There is 1 vowel in "${str}"`;
 
-    } else {
-        return `There are ${totalVowels.length} vowels in "${str}"`;
-    }
+//     } else {
+//         return `There are ${totalVowels.length} vowels in "${str}"`;
+//     }
 
-}
+// }
 
-console.log(countVowels(userInput));
+// console.log(countVowels(userInput));
