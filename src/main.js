d6d8a5c583d11bbee4befd3771c579b83c44c194
Daniel Berrones <Daniel.A.Berrones@gmail.com>
2020-10-24 01:24:41 -0500

    Refactored code

--- src/main.js
+++ src/main.js
@@ -11,13 +11,16 @@ function getString() {
 
     if (totalVowels === null) {
         alert("Please enter a string");
+        document.getElementById("outputText").style.padding = "15px";
         document.getElementById("outputText").innerHTML = 'Please enter a string';
 
     } else if (totalVowels.length === 1) {
+        document.getElementById("outputText").style.padding = "15px";
         document.getElementById("outputText").innerHTML = `You wrote: ${str}`;
         document.getElementById("outputLength").innerHTML = `There is 1 vowel in "${str}"`;
 
     } else {
+        document.getElementById("outputText").style.padding = "15px";
         document.getElementById("outputText").innerHTML = `You wrote: ${str}`;
         document.getElementById("outputLength").innerHTML = `There are ${totalVowels.length} vowels in "${str}"<br>They are ${totalVowels}`;
 
@@ -28,8 +31,14 @@ function clearText() {
     if (document.getElementById("userText").value) {
         document.getElementById("userText").value = '';
         document.getElementById("outputText").innerHTML = '';
+        document.getElementById("outputText").style.padding = 0;
         document.getElementById("outputLength").innerHTML = '';
     } else {
         alert("false");
     }
 }
+
+
+document.getElementById("outputText").style.padding = 0;
+
+
